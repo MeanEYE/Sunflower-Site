376649597b1286c7910c45983fa00dfb307c1977
Mladen Mijatov <meaneye.rcf@gmail.com>
2014-08-20 23:59:37 +0300

    Added scripts.

---
+++ scripts/gallery.js
@@ -0,0 +1,11 @@
+/**
+ * Gallery Implementation JavaScript
+ * Sunflower File Manager
+ *
+ * Copyright (c) 2014. by Way2CU, http://way2cu.com
+ * Authors: Mladen Mijatov
+ */
+
+$(function() {
+	new LightBox('a.screenshot', true, false, true);
+});
---
+++ scripts/main.js
@@ -0,0 +1,7 @@
+/**
+ * Main JavaScript
+ * Sunflower File Manager
+ *
+ * Copyright (c) 2014. by Mladen Mijatov
+ * Authors: Mladen Mijatov
+ */
